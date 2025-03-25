85921534727f6a898b75cd12423354ad565b692b
Manash Dutta <manash_dutta@msn.com>
2025-03-25 21:36:26 +0530

    Add responsiveness for different screen sizes

--- src/Components/QuotesApp.jsx
+++ src/Components/QuotesApp.jsx
@@ -1,16 +1,49 @@
+import { useState } from "react";
+
 const QuotesApp = () => {
+  const [quote, setQuote] = useState({
+    text: "Ask not what your country can do for you, ask what you can do for your country.",
+    author: "John Fitzgerald Kennedy",
+  });
+
+  const [favorites, setFavorites] = useState([]);
+
+  const [showFavorites, setShowFavorites] = useState(false);
+
+  const fetchNewQuote = async () => {
+    // const url = "https://www.quoterism.com/api/quotes/random";
+    const url = "https://api.quotable.io/random";
+    const response = await fetch(url);
+    const data = await response.json();
+    setQuote({
+      text: data.content,
+      author: data.author,
+    });
+  };
+
+  const toggleFavorites = () => setShowFavorites(!showFavorites);
+
+  const addToFavorites = () => {
+    const isAlreadyInFavorite = favorites.some(
+      (fav) => fav.text === quote.text && fav.author === quote.author
+    );
+    !isAlreadyInFavorite && setFavorites([...favorites, quote]);
+  };
+
+  const deleteFavorite = (index) => {
+    const updatedFavorites = favorites.filter((_, i) => i !== index);
+    setFavorites(updatedFavorites);
+  };
+
   return (
     <div className="container">
       <div className="quotes-app">
         <h1 className="app-heading">Quote.</h1>
-        <i className="bx bxs-heart fav-icon"></i>
+        <i className="bx bxs-heart fav-icon" onClick={toggleFavorites}></i>
         <div className="quote">
           <i className="bx bxs-quote-alt-left left-quote"></i>
-          <p className="quote-text">
-            Ask not what your country can do for you, ask what you can do for
-            your country.
-          </p>
-          <p className="quote-author">J. F. Kennedy</p>
+          <p className="quote-text">{quote.text}</p>
+          <p className="quote-author">{quote.author}</p>
           <i className="bx bxs-quote-alt-right right-quote"></i>
         </div>
         <div className="circles">
@@ -20,9 +53,34 @@ const QuotesApp = () => {
           <div className="circle-4"></div>
         </div>
         <div className="buttons">
-          <button className="btn btn-new">New Quote</button>
-          <button className="btn btn-fav">Add to Favorites</button>
+          <button className="btn btn-new" onClick={fetchNewQuote}>
+            New Quote
+          </button>
+          <button className="btn btn-fav" onClick={addToFavorites}>
+            Add to Favorites
+          </button>
         </div>
+        {showFavorites && (
+          <div className="favorites">
+            <button className="btn-close" onClick={toggleFavorites}>
+              <i className="bx bx-x"> </i>
+            </button>
+            {favorites.map((favQuote, index) => (
+              <div className="fav-quote" key={index}>
+                <div
+                  className="fav-quote-delete"
+                  onClick={() => deleteFavorite(index)}
+                >
+                  <i className="bx bx-x-circle"></i>
+                </div>
+                <div className="fav-quote-content">
+                  <div className="fav-quote-text">{favQuote.text}</div>
+                  <div className="fav-quote-author">{favQuote.author}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
